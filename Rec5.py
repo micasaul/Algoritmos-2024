a8c0a3188b9e14f6d111abd9f1538f339e199d7f
Micaela Saul <144958496+micasaul@users.noreply.github.com>
2024-04-09 21:16:23 -0300

    Update Rec5.py

--- Rec5.py
+++ Rec5.py
@@ -1,3 +1,4 @@
+# 5. Desarrollar una función que permita convertir un número romano en un número decimal.
 def romano (num):
     dicc = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
 
@@ -6,11 +7,7 @@ def romano (num):
     else:
         if dicc[num[0]] >= dicc[num[1]]:
             return dicc[num[0]] + romano(num[1:])
- #siguen el ciclo comun pero debo ubicar el indice 0 del num dentro del dicc
-        #para reconocer cual es mayor
-               
         else:
             return romano(num[1:]) - dicc[num[0]] 
- #los numeros mas chicos adelante de los mas grandes restan en romano
 
 print (romano("MCMLXXII"))
